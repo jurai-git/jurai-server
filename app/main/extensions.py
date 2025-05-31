bf8edcf9e4b4b967c201bde051ea321075776048
mgcvale <miguelcvalealt@gmail.com>
2025-05-31 18:24:33 -0300

    update docker-compose redis settings and env variables

--- app/main/extensions.py
+++ app/main/extensions.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
+import os
 
 db = SQLAlchemy()
-redis: Redis = Redis(host='localhost', port=6379, decode_responses=True)
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis: Redis = Redis.from_url(redis_url, decode_responses=True)
