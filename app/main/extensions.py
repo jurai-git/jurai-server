e201814dcfff985e058a69e4522c1ea0cf371a88
mgcvale <miguelcvalealt@gmail.com>
2025-05-31 20:12:46 -0300

    fix: set default redis URL to redis://redis on production branch

--- app/main/extensions.py
+++ app/main/extensions.py
@@ -3,5 +3,5 @@ from redis import Redis
 import os
 
 db = SQLAlchemy()
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://redis:6379')
 redis: Redis = Redis.from_url(redis_url, decode_responses=True)
