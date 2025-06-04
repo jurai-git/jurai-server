118cfbb6306bf5bf6504a25eb783d6580db03dd5
mgcvale <miguelcvalealt@gmail.com>
2025-06-04 14:51:33 -0300

    remove merge artifacts from extensions.py

--- app/main/extensions.py
+++ app/main/extensions.py
@@ -3,11 +3,8 @@ from redis import Redis
 import os
 from dotenv import load_dotenv
 
-<<<<<<< HEAD
 db: SQLAlchemy = SQLAlchemy()
 load_dotenv()
-=======
 db = SQLAlchemy()
->>>>>>> ada6bee4b4e871e6fc739d26a562c0498d9b1ff1
 redis_url = os.getenv('REDIS_URL', 'redis://redis:6379')
 redis: Redis = Redis.from_url(redis_url, decode_responses=True)
